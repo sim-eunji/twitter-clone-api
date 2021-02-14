545f5f29cb9379da4fe75a166bc47f66a6a88ee2
sim-eunji <kingeunji12@gmail.com>
2021-02-14 17:44:09 +0900

    Typescript + Express.js 서버 세팅

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.get("/", function (req, res, next) {
+    res.send("hello typescript express!");
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
